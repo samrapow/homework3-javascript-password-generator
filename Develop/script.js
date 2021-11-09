dfc6c1085c0bf83f666058096b8f0f8b0c487435
Sam Rapowitz <samrapow@gmail.com>
2021-11-08 20:28:52 -0500

    Finished password user prompts

--- Develop/script.js
+++ Develop/script.js
@@ -13,10 +13,11 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
 // Prompt the user for a password length when the Generate Password button is clicked
+// make passwordLength a global variable
+var passwordLength;
 function getPasswordLength() {
-  var passwordLength = prompt("Please enter a password length that is an integer between 8 and 128 characters");
+  passwordLength = prompt("Please enter a password length that is an integer between 8 and 128 characters");
   // Check if the inputted string is an integer between 8 and 128. If it isn't, reprompt the user
   if (passwordLength != null && Number.isInteger(parseFloat(passwordLength)) && parseInt(passwordLength) >=8 && parseInt(passwordLength) <=128)
   {
@@ -37,9 +38,11 @@ function getPasswordLength() {
   }
 }
 
+
 // WHEN asked for character types to include in the password THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+var isLowercase;
 function confirmLowercaseCharacters() {
-  var isLowercase = prompt("Please respond with \"Yes\" or \"No\" to indicate whether you would like to include lowercase characters")
+  isLowercase = prompt("Please respond with \"Yes\" or \"No\" to indicate whether you would like to include lowercase characters")
   if (isLowercase === "Yes" || isLowercase === "No") {
     // THEN my input should be validated
     var proceed = confirm("Are you sure that you would like to keep your answer as \"" + isLowercase + "\" for including lowercase characters?");
@@ -58,8 +61,9 @@ function confirmLowercaseCharacters() {
   }
 }
 
+var isUppercase;
 function confirmUppercaseCharacters() {
-  var isUppercase = prompt("Please respond with \"Yes\" or \"No\" to indicate whether you would like to include uppercase characters")
+  isUppercase = prompt("Please respond with \"Yes\" or \"No\" to indicate whether you would like to include uppercase characters")
   if (isUppercase === "Yes" || isUppercase === "No") {
     // THEN my input should be validated
     var proceed = confirm("Are you sure that you would like to keep your answer as \"" + isUppercase + "\" for including uppercase characters?");
@@ -78,8 +82,9 @@ function confirmUppercaseCharacters() {
   }
 }
 
+var isNumeric;
 function confirmNumericCharacters() {
-  var isNumeric = prompt("Please respond with \"Yes\" or \"No\" to indicate whether you would like to include numeric characters")
+  isNumeric = prompt("Please respond with \"Yes\" or \"No\" to indicate whether you would like to include numeric characters")
   if (isNumeric === "Yes" || isNumeric === "No") {
     // THEN my input should be validated
     var proceed = confirm("Are you sure that you would like to keep your answer as \"" + isNumeric + "\" for including numeric characters?");
@@ -98,14 +103,16 @@ function confirmNumericCharacters() {
   }
 }
 
+var isSpecial;
 function confirmSpecialCharacters() {
-  var isSpecial = prompt("Please respond with \"Yes\" or \"No\" to indicate whether you would like to include special characters")
+  isSpecial = prompt("Please respond with \"Yes\" or \"No\" to indicate whether you would like to include special characters")
   if (isSpecial === "Yes" || isSpecial === "No") {
     // THEN my input should be validated
     var proceed = confirm("Are you sure that you would like to keep your answer as \"" + isSpecial + "\" for including special characters?");
     if (proceed) {
-      // print the answer to the screen 
-      document.getElementById("password").innerHTML = isSpecial;
+      // make sure that at least one character type is selected
+      // selectAtLeastOne();
+      selectAtLeastOne();
       return;
     } else {
       confirmSpecialCharacters() ;
@@ -117,3 +124,18 @@ function confirmSpecialCharacters() {
     confirmSpecialCharacters();
   }
 }
+
+// and at least one character type should be selected
+function selectAtLeastOne () {
+  if (isLowercase === "No" && isUppercase === "No" && isNumeric === "No" && isSpecial === "No") {
+    var restart = alert("You must respond \"Yes\" to at least one character type. Please choose again");
+    // start choosing lowercase characters again
+    confirmLowercaseCharacters();
+    return;
+  } else {
+    document.getElementById("password").innerHTML = isSpecial;
+    return;
+  }
+}
+
+// THEN a password is generated that matches the selected criteria
